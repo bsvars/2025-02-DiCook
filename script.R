69c5965bb1eb4020330c092633d45bfd4b9943d9
adamwang15 <adamwang15@gmail.com>
2025-02-17 17:34:58 +1100

    first few slides

--- script.R
+++ script.R
@@ -2,20 +2,27 @@ library(bsvarSIGNs)
 
 data <- readxl::read_excel("ecor12749-sup-0001-supinfo/VARData.xlsx")
 
+# endogenous variables
 Y <- as.matrix(data[, 2:5])
+
+# exogenous variables
 Z <- data[, 6:8] |>
   as.matrix() |>
   bsvars::specify_data_matrices$new(p = 4)
-Z <- rbind(matrix(0, 4, 12), t(Z$X[-nrow(Z$X), ]))
+Z <- rbind(matrix(0, 4, 12), t(Z$X[-nrow(Z$X), ])) # pad with zeros
 
+# sign restrictions of +ve monetary policy shock
+# restrictions on impulse response functions
 sign_irf <- matrix(NA, 4, 4)
-sign_irf[1, 1] <- sign_irf[4, 1] <- 1
-sign_irf[3, 1] <- -1
-sign_irf <- array(sign_irf, c(4, 4, 4))
+sign_irf[1, 1] <- sign_irf[4, 1] <- 1 # +ve impact on cash rate and exchange rate
+sign_irf[3, 1] <- -1 # -ve impact on consumer price index
+sign_irf <- array(sign_irf, c(4, 4, 4)) # last for 4 periods
 
+# restrictions on policy reaction function
 sign_structural <- matrix(NA, 4, 4)
 sign_structural[1, ] <- c(NA, -1, -1, 1)
 
+# estimate the model
 spec <- specify_bsvarSIGN$new(
   Y,
   p = 4,
